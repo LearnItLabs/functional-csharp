c8e274f5effc606f48eea8ab3dc7d05c9d143f2f
Walt Ritscher <waltritscher@outlook.com>
2024-08-13 10:30:18 -0700

    update comments

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -7,9 +7,12 @@ namespace ConsoleApp
 {
 	public class Examples
 	{
-		// Pure functions are similar to mathematical functions
-		// they compute an output value based on their input values. 
-		// they do nothing else in the code
+
+
+		// Pure functions are like mathematical functions:
+		// they compute an output value based solely on their input values.
+		// They perform no other actions in the code.
+
 		public decimal CalcDiscount(decimal amount, decimal discountRate)
 		{
 			return amount * (1 - discountRate);
