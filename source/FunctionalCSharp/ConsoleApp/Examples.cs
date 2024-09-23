4f04e41404325c5e6d131a2b3fcd455d0bfd35e1
Walt Ritscher <waltritscher@outlook.com>
2024-09-22 20:00:27 -0700

    update create method

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -1,28 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 
-namespace ConsoleApp
-{
-	internal class Examples
-	{
-		public void AggregateExample()
-		{
-			// function fold (or reduce)
-			// fold applies a function to each item in the list
-			// it takes a list, and returns a single result (not a list)
+namespace ConsoleApp {
+	internal class Examples {
+		public void AggregateExample() {
+			// Function fold (or reduce)
+			// Applies a function to each item in the list
+			// Takes a list and returns a single result (not a list)
 			// It is an accumulative function
-			// for example, SUM totals the 1st and 2nd item 
-			// then totals that result with the 3rd item and so on...
+			// For example, SUM totals the 1st and 2nd item,
+			// then totals that result with the 3rd item, and so on...
 			// 
 			// LINQ (Aggregate, Sum, Average)
 			// LINQ (Max, Min, Count)
 
-			ImmutableList<int> setA = ImmutableList.Create(5, 4, 1, 3, 9, 8, 6, 7, 2, 12, 24);
-			ImmutableList<int> setB = 
-				ImmutableList.Create(Enumerable.Range(1, 40).Where(x => x % 5 == 0).ToArray());
+			ImmutableList<int> setA = [5, 4, 1, 3, 9, 8, 6, 7, 2, 12, 24];
+			
+			// list of numbers 5 through 40 (by five)
+			ImmutableList<int> setB =
+				ImmutableList.CreateRange(Enumerable.Range(1, 40).Where(x => x % 5 == 0));
 
 			// predefined aggregates
 			var total = setA.Sum();
@@ -40,18 +37,17 @@ namespace ConsoleApp
 			var anotherMultiple = setA.Aggregate(100, (first, second) => first * second);
 		}
 
-		public void AggregateRobots()
-		{
-			
+		public void AggregateRobots() {
+
 			var robot1 = new Robot(name: "Robot1", batteryLevel: 60);
 			var robot2 = new Robot(name: "Robot2", batteryLevel: 45);
 			var robot3 = new Robot(name: "Robot3", batteryLevel: 95);
 			var robot4 = new Robot(name: "Robot4", batteryLevel: 27);
 
-			ImmutableList<Robot> robots = ImmutableList.Create(new Robot[] { robot1, robot2 , robot3, robot4});
+			ImmutableList<Robot> robots = ImmutableList.Create(new Robot[] { robot1, robot2, robot3, robot4 });
 
 			var lowBattery = robots.Min(x => x.BatteryLevel);
-	
+
 		}
 
 	}
@@ -59,12 +55,10 @@ namespace ConsoleApp
 
 
 
-	public class Robot
-	{
+	public class Robot {
 		public string Name { get; }
 		public int BatteryLevel { get; }
-		public Robot(string name, int batteryLevel)
-		{
+		public Robot(string name, int batteryLevel) {
 			Name = name;
 			BatteryLevel = batteryLevel;
 		}
