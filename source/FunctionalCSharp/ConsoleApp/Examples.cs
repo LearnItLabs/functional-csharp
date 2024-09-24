799bdd49a1a5384ed4e4e47410f498ce3a5a816d
walt-instructor <staffauthor@outlook.com>
2024-09-23 23:01:57 -0700

    format code

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -43,21 +43,18 @@ namespace ConsoleApp {
 			Console.WriteLine($"{result1.Value},{result2.Value},{result3.Value}");
 			Console.WriteLine(new String('=', 60));
 		}
-
 		public void CreateRandomOtherTypes() {
 			var bResult = Gen.Next(DateTime.Now.Ticks).AsBool();
 			var dResult = Gen.Next(DateTime.Now.Ticks).AsDouble();
 			var cResult = Gen.Next(DateTime.Now.Ticks).AsChar();
 		}
-
 		public void CreateARandomList() {
 
 			var list = Gen.CreateRandomSet(seed: 123, count: 10,
 																								 low: -125, high: 120).ToList();
 		}
-
 		public void OrderListRandomly() {
-			var numbers = Enumerable.Range(10,30);
+			var numbers = Enumerable.Range(10, 30);
 			var results = Gen.ReorderSet(seed: 423, currentSet: numbers);
 		}
 	}
